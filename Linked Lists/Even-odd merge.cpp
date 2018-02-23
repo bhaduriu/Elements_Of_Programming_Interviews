0a642b88da7e49d370173efe50e4e067df8e70a1
Uddalak Bhaduri <bhaduriu@gmail.com>
2018-02-23 22:54:32 +0530

    Create Even-odd merge.cpp

---
+++ Linked Lists/Even-odd merge.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <malloc.h>
+using namespace std;
+struct node{
+    int data;
+    struct node* next;
+};
+struct node* newnode(int data)
+{
+    struct node* temp = (struct node*)malloc(sizeof(struct node));
+    temp->data=data;temp->next=NULL;
+    return temp;
+}
+void append(struct node* head, int data)
+{
+    if(head!=NULL)
+    {
+        struct node* p = head;
+        while(p->next!=NULL)
+            p=p->next;
+        struct node* temp = newnode(data);
+        p->next=temp;
+    }
+}
+void print(struct node * head)
+{
+    struct node *t = head;
+    while(t!=NULL)
+    {
+        cout<<t->data<<"->";
+        t=t->next;
+    }
+    cout<<"NULL"<<endl;
+}
+struct node* evenodd(struct node* head)
+{
+    struct node* p=head,*q=head,*m=head;int c=0;
+    while(m->next!=NULL)
+        m=m->next;
+    //O(n)
+    struct node* temp=m;
+    if(m->data%2)//odd
+        c=m->data;
+    while(p!=temp)
+    {
+        if(p->data%2)//odd
+        {
+            if(p==q)
+            {
+                p=q->next;
+                m->next=newnode(q->data);
+                m=m->next;
+                free(q);
+                q=p;
+                head=q;
+            }
+            else
+            {
+                q->next=p->next;
+                m->next=newnode(p->data);
+                m=m->next;
+                free(p);
+                p=q->next;
+            }
+        }
+        else
+        {
+            q=p;p=p->next;
+        }
+    }
+    //O(n)
+    struct node* mm=head;
+    if(c!=0)
+    {
+        m->next=newnode(p->data);
+        while((mm->next)!=temp)
+            mm=mm->next;
+        //O(n)
+        mm->next=temp->next;
+    }
+    free(p);free(q);free(m);
+    free(mm);free(temp);
+    return head;
+}
+int main()
+{
+    struct node* head=NULL;
+    head=newnode(17);
+    append(head,15);
+    append(head,8);
+    append(head,12);
+    append(head,10);
+    append(head,5);
+    append(head,4);
+    append(head,1);
+    append(head,7);
+    append(head,6);
+    print(head);
+    head=evenodd(head);
+    print(head);
+    return 0;
+}
+
+
