2e386c8bd8e6c50da3af3c80f61fa8810d0e8099
Uddalak Bhaduri <bhaduriu@gmail.com>
2018-02-27 13:40:53 +0530

    Create Find MIN and MAX simultaneously.cpp

---
+++ Searching/Find MIN and MAX simultaneously.cpp
@@ -0,0 +1,45 @@
+/*
+Partition the array into min candidates and max candidates by comparing successive pairs,
+this will give (n/2)-1 comparisons to find the min from the min candidates and (n/2)-1 
+comparisons to find the max from the max candidates, yielding a total of (3n/2)-2 comparisons
+*/
+
+#include <iostream>
+#include <limits.h>
+#define N 6
+using namespace std;
+
+int main()
+{
+    int a[N]={3,2,5,1,2,4};
+    int max=INT_MIN,min=INT_MAX;
+    for(int i=0;i<N;i+=2)
+    {
+        if(i+1==N)//checking for odd sized array
+        {
+            if(a[i]>max)
+                max=a[i];
+            else
+                min=a[i];
+        }
+        else// for all other even sized array
+        {
+            if(a[i]>a[i+1])
+            {
+                if(a[i]>max)
+                    max=a[i];
+                if(a[i+1]<min)
+                    min=a[i+1];
+            }
+            else
+            {
+                if(a[i+1]>max)
+                    max=a[i+1];
+                if(a[i]<min)
+                    min=a[i];
+            }
+        }
+    }
+    cout<<"Max: "<<max<<" and Min: "<<min<<endl;
+    return 0;
+}
